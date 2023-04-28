003e7223e15bd00849c4b0dd8a54d89549361312
user1 <user@email.com>
2023-04-28 15:19:49 +0200

    Correzione calcolo

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ eta = parseInt(prompt ("eta"));
 prezzo = km * 0.21;
 console.log(prezzo);
 if (eta < 18) {
-    prezzo = ( prezzo * 20 ) / 100;
+    prezzo = prezzo - ( prezzo * 20 ) / 100;
 } else if (eta >= 65) {
-    prezzo = ( prezzo * 40 ) / 100;
+    prezzo = prezzo - ( prezzo * 40 ) / 100;
 }
 document.getElementById('prezzo').innerHTML = "Il prezzo è " + prezzo.toFixed(2) + "€";
 console.log(
