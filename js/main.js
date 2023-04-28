8591dfba134957ee8b9ad1167e0d03b17a98c0a9
user1 <user@email.com>
2023-04-28 12:14:51 +0200

    Completato.

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@ prezzo = km * 0.21;
 console.log(prezzo);
 if (eta < 18) {
     prezzo = ( prezzo * 20 ) / 100;
-} 
-if (eta >= 65) {
+} else if (eta >= 65) {
     prezzo = ( prezzo * 40 ) / 100;
 }
 document.getElementById('prezzo').innerHTML = "Il prezzo è " + prezzo.toFixed(2) + "€";
